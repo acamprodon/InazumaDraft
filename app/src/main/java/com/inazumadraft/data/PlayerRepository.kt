9bfef4439606dc1eae3a88ea5d59eeb09f7b2167
acamprodon <camprodonandres@gmail.com>
2025-10-11 15:51:13 +0200

    xd

--- app/src/main/java/com/inazumadraft/data/PlayerRepository.kt
+++ app/src/main/java/com/inazumadraft/data/PlayerRepository.kt
@@ -6,20 +6,26 @@ import com.inazumadraft.model.Player
 object PlayerRepository {
 
     val players = listOf(
-        Player("Mark Evans", "PT", 85, 70, 80, R.drawable.mark),
-        Player("Axel Blaze", "DL", 90, 85, 88, R.drawable.axel),
-        Player("Jude Sharp", "MC", 78, 82, 91, R.drawable.jude),
-        Player("Nathan Swift", "DF", 80, 95, 75, R.drawable.nathan),
-        Player("Shawn Froste", "DL", 88, 84, 86, R.drawable.shawn),
-        Player("Byron Love", "MC", 82, 80, 90, R.drawable.byron),
-        Player("Jack Wallside", "DF", 85, 60, 70, R.drawable.jack),
-        Player( "Joseph King", "PT", 80, 75, 79, R.drawable.king),
-        Player ("Bobby Shearer", "DF", 80, 70, 80, R.drawable.bobby),
-        Player ("Erik", "MC", 82,80,82, R.drawable.erik),
-        Player ("Kevin Dragonfly" , "DL", 80, 75, 75, R.drawable.kevin),
-        Player("Shadow", "DL", 90, 90, 90, R.drawable.shadow),
-        Player("Caleb", "MC", 80, 79, 88, R.drawable.caleb),
-        Player("Archer", "DF", 83, 73, 65, R.drawable.archer)
+        Player("Mark Evans", "PT",R.drawable.earth , 72, 68,75, 87,  R.drawable.mark),
+        Player("Axel Blaze", "DL", R.drawable.fuego,90, 80, 85,65,  R.drawable.axel),
+        Player("Jude Sharp", "MC",R.drawable.aire, 75, 82, 91,80,  R.drawable.jude),
+        Player("Nathan Swift", "DF", R.drawable.aire, 76, 95, 70,70,  R.drawable.nathan),
+        Player("Shawn Froste", "DL", R.drawable.aire,88, 87, 80,78,  R.drawable.shawn),
+        Player("Byron Love", "MC", R.drawable.bosque,85, 80, 90, 68, R.drawable.byron),
+        Player("Jack Wallside", "DF", R.drawable.earth, 62, 49, 70,85,  R.drawable.jack),
+        Player( "Joseph King", "PT",R.drawable.fuego , 68, 75, 79,84,  R.drawable.king),
+        Player ("Bobby Shearer", "DF", R.drawable.bosque,70, 70, 78,78,  R.drawable.bobby),
+        Player ("Erik", "MC",R.drawable.bosque,  82,80,82, 70, R.drawable.erik),
+        Player ("Kevin Dragonfly" , "DL",R.drawable.bosque, 80, 75, 75,62, R.drawable.kevin),
+        Player("Shadow", "DL", R.drawable.bosque,90, 90, 90, 80, R.drawable.shadow),
+        Player("Caleb", "MC", R.drawable.fuego,80, 79, 88, 75,R.drawable.caleb),
+        Player("Archer", "DF", R.drawable.bosque,83, 73, 65,75,  R.drawable.archer),
+        Player( "Jim", "DF",R.drawable.bosque, 60, 55, 75, 60, R.drawable.jim ),
+        Player("Max", "DL", R.drawable.aire, 70, 75, 78, 70, R.drawable.max),
+        Player("Sam", "MC", R.drawable.fuego, 71, 70, 75, 71, R.drawable.sam),
+        Player("Steve", "MC", R.drawable.aire, 68, 72, 78, 70, R.drawable.steve),
+        Player("Timmy", "MC", R.drawable.bosque, 75, 75, 75, 70, R.drawable.timmy),
+        Player("Tod","DF", R.drawable.fuego, 68, 73, 74, 75, R.drawable.tod ),
 
     )
 
--- app/src/main/java/com/inazumadraft/model/Player.kt
+++ app/src/main/java/com/inazumadraft/model/Player.kt
@@ -6,9 +6,11 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Player(
     val name: String,
-    val position: String, // "Portero", "Defensa", "Centrocampista", "Delantero"
-    val power: Int,
+    val position: String,
+    val element: Int,
+    val kick: Int,
     val speed: Int,
-    val technique: Int,
+    val control: Int,
+    val defense: Int,
     val image: Int
 ) : Parcelable
