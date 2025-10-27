cc43ce024bac16b4d968e5e7df83313efa7a4125
acamprodon <camprodonandres@gmail.com>
2025-10-27 14:52:09 +0100

    xd

--- app/src/main/java/com/inazumadraft/model/Tecnica.kt
+++ app/src/main/java/com/inazumadraft/model/Tecnica.kt
@@ -6,9 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Tecnica(
     val name: String,
-    val type: String,
     val power: Int,
-    val element: Int,
     val players: List<String>,
     val combined: Boolean = false
 
